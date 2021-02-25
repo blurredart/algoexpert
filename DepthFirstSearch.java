a1866814d6b34ecd598410d37c4f83091f76765e
blurredart <34162174+blurredart@users.noreply.github.com>
2021-02-25 19:27:56 +0530

    Create DepthFirstSearch.java

---
+++ DepthFirstSearch.java
@@ -0,0 +1,31 @@
+import java.util.*l
+class Program
+{
+  static class Node 
+  {
+    String name;
+    List<Node> children = new ArrayList<Node>();
+    
+    public Node(String name)
+    {
+      this.name = name;
+    }
+    
+    public List<String> depthFirstSearch(List<String> array)
+    {
+      array.add(this.name);
+      for (Node child:this.children)
+      {
+        child.depthFirstSearch(array);
+      }
+      return arrayl
+    }
+    
+    public Node addChild(String name)
+    {
+      Node child = new Node(name);
+      children.add(child);
+      return this;
+    }
+  }
+}
